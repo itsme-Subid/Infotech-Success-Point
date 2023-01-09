754bebda76c25211ba893efac209f9974e4771fd
SUBID DAS <itsmesubid@gmail.com>
2023-01-09 19:24:36 +0530

    Minor `bug fix`

--- pages/api/courses.ts
+++ pages/api/courses.ts
@@ -9,7 +9,7 @@ export default function handler(
   }>
 ) {
   try {
-    readFile("./courseDetails/courses.json", "utf8", (err, data) => {
+    readFile("./public/courseDetails/courses.json", "utf8", (err, data) => {
       if (!!err) {
         res.status(500).json({ error: err.message });
       } else {
